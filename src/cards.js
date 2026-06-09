fb0905dcb3eb3cd48444ec9568059fae4831216b
CyrilHermann <cyrilhermanntrash@gmail.com>
2026-06-09 23:05:12 +0200

    Create cards.js

---
+++ src/cards.js
@@ -0,0 +1,21 @@
+export const cards = {
+  matins: {
+    title: "5 matins"
+  },
+
+  transitionNuit: {
+    title: "Transition Jour → Nuit"
+  },
+
+  nuits: {
+    title: "3 ou 4 nuits"
+  },
+
+  retourJour: {
+    title: "Retour au rythme de jour"
+  },
+
+  joursLongs: {
+    title: "Journées longues et après-midi"
+  }
+};
