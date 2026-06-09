dcf01eefd132ed354d2d394eeeff59fb702666d2
CyrilHermann <cyrilhermanntrash@gmail.com>
2026-06-10 01:30:28 +0200

    Update excelParser.js

--- src/excelParser.js
+++ src/excelParser.js
@@ -1,20 +1,30 @@
 export function parseExcel(workbook) {
-
   console.log("Excel Parser lancé");
 
   const firstSheetName = workbook.SheetNames[0];
-
   const worksheet = workbook.Sheets[firstSheetName];
 
-  const jsonData = XLSX.utils.sheet_to_json(
-    worksheet,
-    { header: 1 }
-  );
+  const coloredCells = [];
 
-  console.log("Nombre de lignes :", jsonData.length);
+  Object.keys(worksheet).forEach((cellAddress) => {
+    if (cellAddress.startsWith("!")) return;
+
+    const cell = worksheet[cellAddress];
+
+    if (cell.s && cell.s.fgColor) {
+      coloredCells.push({
+        cell: cellAddress,
+        value: cell.v,
+        color: cell.s.fgColor.rgb || cell.s.fgColor.indexed || cell.s.fgColor.theme
+      });
+    }
+  });
+
+  console.log("Cellules colorées :", coloredCells.slice(0, 50));
 
   return {
     sheetName: firstSheetName,
-    totalRows: jsonData.length
+    coloredCellsCount: coloredCells.length,
+    sample: coloredCells.slice(0, 20)
   };
 }
