34408ff3f743bd1a90c3ddd9b16456ba3027fbe3
CyrilHermann <cyrilhermanntrash@gmail.com>
2026-06-10 01:28:07 +0200

    Update excelParser.js

--- src/excelParser.js
+++ src/excelParser.js
@@ -1,9 +1,20 @@
 export function parseExcel(workbook) {
+
   console.log("Excel Parser lancé");
 
   const firstSheetName = workbook.SheetNames[0];
 
   const worksheet = workbook.Sheets[firstSheetName];
 
-  return worksheet;
+  const jsonData = XLSX.utils.sheet_to_json(
+    worksheet,
+    { header: 1 }
+  );
+
+  console.log("Nombre de lignes :", jsonData.length);
+
+  return {
+    sheetName: firstSheetName,
+    totalRows: jsonData.length
+  };
 }
