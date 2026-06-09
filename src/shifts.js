acf6060d64ef07bf967dbb8f4014398eee102c3a
CyrilHermann <cyrilhermanntrash@gmail.com>
2026-06-10 00:26:07 +0200

    Create shifts.js

---
+++ src/shifts.js
@@ -0,0 +1,41 @@
+export const shifts = {
+  matin: {
+    start: "06:10",
+    end: "14:10"
+  },
+
+  apresMidi: {
+    start: "14:00",
+    end: "22:10"
+  },
+
+  nuitSemaine: {
+    start: "22:00",
+    end: "06:10"
+  },
+
+  nuitVendredi: {
+    start: "22:00",
+    end: "06:50"
+  },
+
+  samediJour: {
+    start: "06:40",
+    end: "18:40"
+  },
+
+  samediNuit: {
+    start: "18:30",
+    end: "06:30"
+  },
+
+  dimancheJour: {
+    start: "06:20",
+    end: "18:20"
+  },
+
+  dimancheNuit: {
+    start: "18:10",
+    end: "06:10"
+  }
+};
