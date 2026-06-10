91f681cd7e46847bef145b317384b48f687cccd6
CyrilHermann <cyrilhermanntrash@gmail.com>
2026-06-10 02:25:17 +0200

    Update calendar.js

--- src/calendar.js
+++ src/calendar.js
@@ -1,22 +1,18 @@
 export const calendar = {
   "2026-06-08": {
     vert: {
-      phase: "matins",
       shift: "matin"
     },
 
     bleu: {
-      phase: "retourJour",
       shift: "repos"
     },
 
     jaune: {
-      phase: "nuits",
-      shift: "nuitSemaine"
+      shift: "nuit"
     },
 
     rouge: {
-      phase: "transitionJourNuit",
       shift: "repos"
     }
   }
