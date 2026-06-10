ee71b4ed9783f1a127b7cdd55ac814b97c91ffcf
CyrilHermann <cyrilhermanntrash@gmail.com>
2026-06-10 02:18:42 +0200

    Update shifts.js

--- src/shifts.js
+++ src/shifts.js
@@ -1,46 +1,21 @@
-export const shifts = {
-  matin: {
-    start: "06:10",
-    end: "14:10"
-  },
+export function getCardFromShift(shift) {
 
-  apresMidi: {
-    start: "14:00",
-    end: "22:10"
-  },
+  switch (shift) {
 
-  nuitSemaine: {
-    start: "22:00",
-    end: "06:10"
-  },
+    case "matin":
+      return "matins";
 
-  nuitVendredi: {
-    start: "22:00",
-    end: "06:50"
-  },
+    case "apresMidi":
+      return "joursLongs";
 
-  samediJour: {
-    start: "06:40",
-    end: "18:40"
-  },
+    case "nuit":
+      return "nuits";
 
-  samediNuit: {
-    start: "18:30",
-    end: "06:30"
-  },
+    case "jour":
+      return "retourJour";
 
-  dimancheJour: {
-    start: "06:20",
-    end: "18:20"
-  },
-
-  dimancheNuit: {
-    start: "18:10",
-    end: "06:10"
-  },
-  
-  repos: {
-  start: "Repos",
-  end: "Repos"
+    default:
+      return "matins";
   }
-};
+
+}
