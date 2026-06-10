305c7a393df07a2c497d1a5caf804fa378797512
CyrilHermann <cyrilhermanntrash@gmail.com>
2026-06-10 05:14:53 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -14,7 +14,13 @@ async function loadCalendar() {
 }
 
 function getTodayKey() {
-  return "2026-06-08"; // date test pour l’instant
+  const today = new Date();
+
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  const day = String(today.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
 }
 
 function findNextShift(couleur, startDateKey) {
