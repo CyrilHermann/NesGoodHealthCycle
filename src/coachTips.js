f7b73f18e2f03a26300d4356d51a3a2970ab04d8
CyrilHermann <cyrilhermanntrash@gmail.com>
2026-06-16 20:33:17 +0200

    Create coachTips.js

---
+++ src/coachTips.js
@@ -0,0 +1,19 @@
+export const coachTips = {
+  matins: [
+    "Expose-toi à la lumière naturelle dans l'heure suivant ton réveil pour renforcer ton rythme circadien.",
+    "Hydrate-toi dès le réveil avec un grand verre d'eau afin de compenser les pertes nocturnes.",
+    "Privilégie un petit-déjeuner riche en protéines pour soutenir ta vigilance jusqu'au repas de midi."
+  ],
+
+  nuits: [
+  ],
+
+  retourJour: [
+  ],
+
+  joursLongs: [
+  ],
+
+  transitionJourNuit: [
+  ]
+};
