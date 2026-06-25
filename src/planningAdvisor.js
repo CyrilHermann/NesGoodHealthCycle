52f937cc40ca2b8822cea0040faec5f3853fa996
CyrilHermann <cyrilhermanntrash@gmail.com>
2026-06-25 21:58:41 +0200

    Update planningAdvisor.js

--- src/planningAdvisor.js
+++ src/planningAdvisor.js
@@ -1,1 +1,20 @@
 
+export const planningAdvisor = {
+
+  mariage: {
+    matins: [...],
+    transitionJourNuit: [...],
+    nuits: [...],
+    retourJour: [...],
+    joursLongs: [...]
+  },
+
+  vacances: {
+    ...
+  },
+
+  course: {
+    ...
+  }
+
+};
